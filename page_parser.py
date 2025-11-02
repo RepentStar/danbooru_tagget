d22875ec92a617b8cf0dc7c0a2a8624547ce7495
RepentStar <repent@repent.top>
2025-11-02 14:16:19 +0800

    config: Add "commisson" into tag_filter_list.

--- page_parser.py
+++ page_parser.py
@@ -14,6 +14,7 @@ class PageParser:
             [
                 "original",
                 "commentary",
+                "commission",
                 "non-web source",
                 ".*request$",
                 "highres",
